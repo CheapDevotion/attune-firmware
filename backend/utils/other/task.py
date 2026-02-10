f6888ca0ceaa386f8e9b930bb2abf64d6cc59bab
Jacob Williams <cheapdevotion@gmail.com>
2026-02-09 22:16:19 -0600

    Trim repo to XIAO minimal firmware and fix OMI Kconfig symbols

--- backend/utils/other/task.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-
-def safe_create_task(t):
-    task = asyncio.create_task(t)
-    task.add_done_callback(
-        lambda l: print("Unhandled exception in background task:", l.exception()) if l.exception() else None
-    )
-    return task
